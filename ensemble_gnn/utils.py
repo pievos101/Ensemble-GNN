efa4c97087cea77eceddc694a408ce1eececd561
pievos101 <bastianxpfeifer@gmail.com>
2022-11-22 12:04:34 +0100

    utils

--- ensemble_gnn/utils.py
+++ ensemble_gnn/utils.py
@@ -23,3 +23,23 @@ def train_test_split(gnnsubnet):
 	gnn2_test.dataset  = test_dataset_list
 
 	return gnn1_train, gnn2_test
+
+def split(gnnsubnet):
+	""""
+	50-50 split 
+	"""
+	gnn1_a  = copy(gnnsubnet)
+	gnn2_b  = copy(gnnsubnet)
+
+	dataset_list = copy(gnnsubnet.dataset)
+	random.shuffle(dataset_list)
+	list_len = len(dataset_list)
+	set_len = int(list_len * 1 / 2)
+
+	a_dataset_list = dataset_list[:set_len]
+	b_dataset_list = dataset_list[set_len:]
+	gnn1_a.dataset = a_dataset_list
+	gnn2_b.dataset = b_dataset_list
+
+	return gnn1_a, gnn2_b
+
